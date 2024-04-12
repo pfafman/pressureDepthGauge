5f1f5ba18c1bb9327e50bbc81d570217153f5b1c
Timothy Pfafman <tim@pfafman.com>
2024-04-12 14:29:32 -0600

    enable post

--- readPressure.js
+++ readPressure.js
@@ -28,7 +28,7 @@ postData = (data) => {
     });
 
     req.on('error', (e) => {
-	console.error("postData",e);
+	   console.error("postData",e);
     });
 
     console.log("postData", data);
@@ -52,63 +52,4 @@ while (true) {
         console.log("Error:", error.message);
     }
 }
-
-raspi.init(() => {
-
-    console.log("Raspi Init: new Serial");
-
-    var serial = new Serial({portId: "/dev/serial0", baudRate: 9600});
-    
-    console.log("Raspi Init: new Serial Created");
-
-    serial.open(() => {
-        console.log("Serial opened ...");
-        
-        serial.on('data', async (byte) => {
-            i++;
-            data.push(byte[0])
-            //console.log("Data:[", byte.length, ']:',i, ':', byte[0]);
-
-            if (data[0] != 255) {
-                i = 0;
-                data = [];
-            }
-            if (i == 4) {
-                let sum = checkSum(data);
-                if (sum != data[3]) {
-                    console.log("Checksum Error", sum, data);
-                } else {
-                    let distance = 0.0393701 * (data[1]*256 + data[2]);
-		            //console.log("Distance", distance);
-                    if ((distance < 0) || (distance > 70)) {
-                        console.log("Bad Value", distance);
-                    } else {
-                        ave += distance;
-                        aveCnt++;
-                        if (minValue > distance) {
-                            minValue = distance;
-                        }
-                        if (maxValue < distance) {
-                            maxValue = distance;
-                        }
-                        if (aveCnt > 100) {
-                            ave /= aveCnt;
-                            if ( ((maxValue - ave) > 1) || ((ave - minValue) > 1) ) {
-                                console.log("Noisy", ave.toFixed(3), minValue.toFixed(3), maxValue.toFixed(3));
-                                resetData();
-                            } else {
-                                console.log("");
-                                console.log("Post distance: (", aveCnt, ") ", ave.toFixed(3), minValue.toFixed(3), maxValue.toFixed(3));
-                                postData({'depth': ave});
-                                resetData();
-                            }
-                        }
-                    }
-                }
-                i = 0;
-                data = [];
-            }
-        });
-    });
-});
 
