56868460170367cc21327972386e0c260e726c57
Timothy Pfafman <tim@pfafman.com>
2024-04-12 14:12:16 -0600

    clean up

--- readPressure.js
+++ readPressure.js
@@ -40,9 +40,9 @@ postData = (data) => {
 
 while (true) {
     try {
-        console.log("run ...");
+        
         const result = execSync('./readSensor.py').toString().trim();
-        console.log(result);
+        //console.log(result);
 
         lines = result.split(/\r?\n/);
         console.log("Send:", lines[1]);
